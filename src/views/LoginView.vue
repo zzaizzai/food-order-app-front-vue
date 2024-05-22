e2e91c038ba13c74d019bfb9a64ee81ff78cb68e
zzaizzai <junsai0530@gmail.com>
2024-05-22 20:51:41 +0900

    login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,7 +8,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import * as authApi from "@/api/auth";
-import axios from "axios";
+import axios, { AxiosError } from 'axios';
 
 export default defineComponent({
   name: "FoodView",
@@ -20,9 +20,23 @@ export default defineComponent({
   },
   components: {},
   methods: {
-    login() {
-        // TODO: create login 
-    },
+    async login() {
+        try {
+            const response = await authApi.login(this.username, this.password);
+            console.log('Response:', response);
+            console.log('Token:', response.data.accessToken);
+        } catch (error: any) {
+            if (error.response) {
+                console.error('Error response data:', error.response.data);
+                console.error('Error response status:', error.response.status);
+                console.error('Error response headers:', error.response.headers);
+            } else if (error.request) {
+                console.error('Error request:', error.request);
+            } else {
+                console.error('Error message:', error.message);
+            }
+        }
+    }
   },
 });
 </script>
