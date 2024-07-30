bcd9e2226f2243537af9c522aa894491dfc589fa
zzaizzai <junsai0530@gmail.com>
2024-07-30 23:33:45 +0900

    url dynamic config

--- src/api/http.ts
+++ src/api/http.ts
@@ -2,8 +2,11 @@ import axios from "axios";
 import store from "@/store";
 
 
+// dynamic config
+const host = window.location.host;
+
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: `http://${host}/api`,
     headers: {
         'Content-Type': 'application/json'
     }
