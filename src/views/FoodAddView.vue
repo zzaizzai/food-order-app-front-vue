ea27fc7654affa510087329baac39cdd6c72162d
zzaizzai <junsai0530@gmail.com>
2024-08-03 14:37:31 +0900

    food add effect

--- src/views/FoodAddView.vue
+++ src/views/FoodAddView.vue
@@ -26,12 +26,17 @@
       ><b-col sm="3"
         ><b-form-input
           placeholder="Category"
-          type="number"
+          type="text"
           v-model="category" /></b-col
     ></b-row>
     <b-row class="justify-content-center"
       ><b-col
-        ><b-button variant="success" @click="addFodd">OK</b-button></b-col
+        ><b-button
+          class="button-color-transition"
+          :variant="buttonVariant"
+          @click="addFodd"
+          >{{ buttonText }}</b-button
+        ></b-col
       ></b-row
     >
   </b-container>
@@ -48,6 +53,7 @@ export default defineComponent({
   name: "FoodAddView",
   data() {
     return {
+      isAdding: false,
       msg: "",
       store: "",
       foodname: "",
@@ -56,13 +62,21 @@ export default defineComponent({
     };
   },
   methods: {
-    async addFodd() {
+    resetAllInput(): void {
+      this.foodname = ""
+      this.store = ""
+      this.category = ""
+      this.price = 0
+    },
+
+    async addFodd(): Promise<void> {
       // reset message
       this.msg = "";
+      this.isAdding = false;
 
       // check
       if (this.store === "" || this.foodname === "" || this.category === "") {
-        this.msg = "pls input";
+        this.msg = "input error";
         return;
       }
 
@@ -73,9 +87,74 @@ export default defineComponent({
         price: this.price,
         description: "",
       };
+      this.isAdding = true;
 
-      const result = await foodApi.addFood(newFood);
+      try {
+        const result = await foodApi.addFood(newFood);
+        console.log(result);
+
+      } catch (error) {
+        console.log("Error", error);
+      }
+
+      // Effect
+      setTimeout(() => {
+        this.isAdding = false;
+        this.msg = "Added a new Food successfully.";
+        this.resetAllInput()
+      }, 2000);
+    },
+  },
+  computed: {
+    buttonVariant(): string {
+      return this.isAdding ? "warning" : "success";
+    },
+    buttonText(): string {
+      return this.isAdding ? "Adding" : "Add";
     },
   },
 });
 </script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* hide spin button */
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+.button-color-transition {
+  transition: background-color 0.5s;
+}
+
+.msgCard {
+  color: red;
+}
+
+.foodcard {
+  padding: 5px;
+  background: #eee;
+  border-radius: 16px;
+  height: 200px;
+  width: 350px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
