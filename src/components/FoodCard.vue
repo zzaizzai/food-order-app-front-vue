c97706918b59d5c8c115c8886b6cededd9cce5c0
zzaizzai <junsai0530@gmail.com>
2024-08-17 00:48:27 +0900

    number format

--- src/components/FoodCard.vue
+++ src/components/FoodCard.vue
@@ -7,13 +7,23 @@
                 </h4>
 
             </b-col>
-            <b-col cols="2"><a :href="`/foodedit?foodId=${food.id}`">Edit</a></b-col>
+            <b-col cols="2"><a :href="`/foodedit?foodId=${food.id}`">
+                    <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="25" height="25">
+                        <path
+                            d="M18.656.93,6.464,13.122A4.966,4.966,0,0,0,5,16.657V18a1,1,0,0,0,1,1H7.343a4.966,4.966,0,0,0,3.535-1.464L23.07,5.344a3.125,3.125,0,0,0,0-4.414A3.194,3.194,0,0,0,18.656.93Zm3,3L9.464,16.122A3.02,3.02,0,0,1,7.343,17H7v-.343a3.02,3.02,0,0,1,.878-2.121L20.07,2.344a1.148,1.148,0,0,1,1.586,0A1.123,1.123,0,0,1,21.656,3.93Z" />
+                        <path
+                            d="M23,8.979a1,1,0,0,0-1,1V15H18a3,3,0,0,0-3,3v4H5a3,3,0,0,1-3-3V5A3,3,0,0,1,5,2h9.042a1,1,0,0,0,0-2H5A5.006,5.006,0,0,0,0,5V19a5.006,5.006,0,0,0,5,5H16.343a4.968,4.968,0,0,0,3.536-1.464l2.656-2.658A4.968,4.968,0,0,0,24,16.343V9.979A1,1,0,0,0,23,8.979ZM18.465,21.122a2.975,2.975,0,0,1-1.465.8V18a1,1,0,0,1,1-1h3.925a3.016,3.016,0,0,1-.8,1.464Z" />
+                    </svg>
+
+                </a></b-col>
             <!-- <b-col cols="2"><b-button pill>X</b-button> </b-col> -->
         </b-row>
         <b-container>
-            
+
             <b-row>
-                {{ food.description ?? "Content Content Content Content Content Content Content Content Content Content" }}
+                <b-col>
+                    {{ food.description ?? "Content Content Content Content Content Content Content Content Content Content" }}
+                </b-col>
 
             </b-row>
             <b-row>
@@ -28,8 +38,8 @@
 
             <b-row><span>store:{{ food.store ?? "store" }}</span></b-row>
             <b-row>
-                <strong>Total Price: <span>{{ totalPrice }}</span>
-                    <span> ({{ food.price }})</span></strong></b-row>
+                <strong>Total Price: <span>{{ formattedTotalPrice }}</span>
+                    <span> ({{ formattedPrice }})</span></strong></b-row>
             <span><strong class="msgCard">{{ msgCard }}</strong></span>
         </b-container>
     </div>
@@ -78,7 +88,6 @@ export default defineComponent({
 
             const order = {
                 foodId: this.food.id,
-                // userId: 6,
                 quantity: this.qty,
                 totalPrice: this.totalPrice,
             };
@@ -116,6 +125,13 @@ export default defineComponent({
         totalPrice(): number {
             return this.food.price * this.qty;
         },
+        formattedTotalPrice(): string {
+            return this.totalPrice.toLocaleString();
+        },
+        formattedPrice(): string {
+            return this.food.price.toLocaleString();
+
+        }
     },
 });
 </script>
